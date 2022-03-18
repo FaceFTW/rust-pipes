683dbbae7f6f168a58952fd89c09dff79faddad2
Alex Westerman <westeraj@rose-hulman.edu>
2022-03-17 20:45:41 -0400

    Have a build no longer requiring X11
    Also some reorganizing bear with me here

---
+++ src/include/pipe.h
@@ -0,0 +1,13 @@
+/**
+ * @file pipe.h
+ * @author Alex "FaceFTW" Westerman
+ * @brief Main Definition of a Pipe
+ * @version 0.1
+ * @date 2022-03-17
+ * 
+ * @copyright Copyright (c) 2022. Work is based on original work from Microsoft Corp (c) 1994
+ * 
+ */
+
+
+
---
+++ src/include/structs.h
@@ -0,0 +1,10 @@
+/**
+ * @file structs.h
+ * @author Alex "FaceFTW" Westerman
+ * @brief Relevant Structs
+ * @version 0.1
+ * @date 2022-03-17
+ * 
+ * @copyright Copyright (c) 2022. Work is based on original work from Microsoft Corp (c) 1994
+ * 
+ */
