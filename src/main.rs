50da01d2d7955e37bbfc711a210cf926e4f4a9a4
Alex Westerman <alex@faceftw.dev>
2023-10-04 12:48:30 -0400

    Converted to new Main Render Loop (Broken atm)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use draw::{make_ball_joint, make_pipe_section};
+use draw::{make_ball_joint, make_pipe_section, RenderObject};
 use rand::Rng;
-use std::{thread::sleep, time::Duration};
 use three_d::{
     degrees, vec3, Axes, Camera, ClearState, DirectionalLight, FrameOutput, OrbitControl, Srgba,
     Window, WindowSettings,
@@ -22,10 +21,9 @@ fn main() {
     let mut rng = rand::thread_rng();
     world.new_pipe(&mut rng);
 
-    //TODO remove these once more logic is in place to remove unused warns
-    world.pipe_update(0, &mut rng);
-    let alive = world.pipes[0].is_alive();
-
+    //===============================================
+    // ENGINE INIT
+    //===============================================
     let window = Window::new(WindowSettings {
         title: "Rust Pipes".to_string(),
         max_size: Some((1280, 720)),
@@ -45,54 +43,17 @@ fn main() {
     );
     let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
 
-    let cylinder = make_pipe_section((0, -1, 0), (0, 0, 0), (0, 255, 0), &context);
-    let cylinder2 = make_pipe_section((0, 0, 0), (1, 0, 0), (255, 0, 0), &context);
-    let joint = make_ball_joint((0, 0, 0), (0, 0, 255), &context);
-
-    let axes = Axes::new(&context, 0.1, 2.0);
+    // let axes = Axes::new(&context, 0.1, 2.0);
     let light0 = DirectionalLight::new(&context, 1.0, Srgba::WHITE, &vec3(0.0, -0.5, -0.5));
     let light1 = DirectionalLight::new(&context, 1.0, Srgba::WHITE, &vec3(0.0, 0.5, 0.5));
+
+    let mut pipe_render_objs: [Vec<RenderObject>; 10] = Default::default();
 
     window.render_loop(move |mut frame_input| {
         camera.set_viewport(frame_input.viewport);
         control.handle_events(&mut camera, &mut frame_input.events);
 
-        frame_input
-            .screen()
-            .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
-            .render(
-                &camera,
-                axes.into_iter()
-                    .chain(&cylinder)
-                    .chain(&cylinder2)
-                    .chain(&joint),
-                &[&light0, &light1],
-            );
-
-        FrameOutput::default()
-    });
-}
-
-//OLD CODE - WILL BE DELETED
-#[cfg(feature = "k3d_engine")]
-fn k3d_run(world: &mut World, mut rng: impl Rng) {
-    extern crate kiss3d;
-    use draw::{draw_axes, make_ball_joint, make_pipe_section};
-    use kiss3d::{camera::ArcBall, nalgebra::Point3, scene::SceneNode, window::Window};
-    //===============================================
-    // KISS3D INITIALIZATION
-    //===============================================
-    let mut window = Window::new("rust-pipes");
-
-    let mut pipe_nodes: Vec<SceneNode> = Vec::new();
-    let eye = Point3::new(-20.0 as f32, 10.0, -20.0);
-    let at_point = Point3::new(10.0 as f32, 10.0, 10.0);
-    let mut camera = ArcBall::new(eye, at_point);
-
-    while window.render_with_camera(&mut camera) {
-        draw_axes(&mut window);
-        sleep(Duration::from_millis(100));
-
+        //World Update Step
         for i in 0..world.active_pipes {
             if !world.pipes[i].is_alive() {
                 continue;
@@ -101,26 +62,41 @@ fn k3d_run(world: &mut World, mut rng: impl Rng) {
 
             if delta_state.last_node != delta_state.current_node {
                 if delta_state.last_dir != delta_state.current_dir {
-                    pipe_nodes.push(make_ball_joint(
+                    pipe_render_objs[i].push(make_ball_joint(
                         delta_state.last_node,
-                        &mut window,
                         delta_state.pipe_color,
+                        &context,
                     ));
                 } else {
-                    pipe_nodes.push(make_pipe_section(
+                    pipe_render_objs[i].push(make_pipe_section(
                         delta_state.last_node,
                         delta_state.current_node,
-                        &mut window,
                         delta_state.pipe_color,
+                        &context,
                     ));
                 }
             }
         }
         if rng.gen_bool(world.new_pipe_chance) && world.active_pipes < MAX_PIPES as usize {
             let data = world.new_pipe(&mut rng);
-            pipe_nodes.push(make_ball_joint(data.start_node, &mut window, data.color));
+            pipe_render_objs[world.active_pipes - 1].push(make_ball_joint(
+                data.start_node,
+                data.color,
+                &context,
+            ));
         }
-    }
+
+        frame_input
+            .screen()
+            .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
+            .render(
+                &camera,
+                pipe_render_objs.iter().flatten(),
+                &[&light0, &light1],
+            );
+
+        FrameOutput::default()
+    });
 }
 
 #[cfg(test)]
