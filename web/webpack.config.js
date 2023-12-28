829b245995a8f8e84da9285a9b79d9931efdbdd7
Alex Westerman <alex@faceftw.dev>
2023-12-28 18:57:47 -0500

    Remove Unnecessary dev-deps

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,7 +1,6 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -14,12 +13,6 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: 'index.html'
-		}),
-		// Have this example work in Edge which doesn't ship `TextEncoder` or
-		// `TextDecoder` at this time.
-		new webpack.ProvidePlugin({
-			TextDecoder: ['text-encoding', 'TextDecoder'],
-			TextEncoder: ['text-encoding', 'TextEncoder']
 		}),
 		new CopyPlugin({
 			patterns: [{
