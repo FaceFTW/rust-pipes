fdb408d3db96333ce56277030fca3af978cf3267
Alex Westerman <alex@faceftw.dev>
2024-01-05 14:23:21 -0500

    Remove logging from WASM (unused)

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,6 @@ cfg_if::cfg_if! {
         use wasm_bindgen::prelude::*;
         #[wasm_bindgen(start)]
         pub async fn start() -> Result<(), JsValue> {
-            console_log::init_with_level(log::Level::Debug).unwrap();
-
-            use log::info;
-            info!("Logging works!");
-
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
             engine::real_main();
             Ok(())
