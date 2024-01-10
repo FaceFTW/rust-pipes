59039cef3ef081b2c15fbb6324559eacd7d69638
Alex Westerman <alex@faceftw.dev>
2024-01-09 19:55:32 -0500

    Add windows_subsystem attribute

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+#![windows_subsystem = "windows"]
 ///This is an entry point for standard executables, basically
 /// invokes the engine facade to do the actual thing.
-
 fn main() {
     rust_pipes_lib::entry_point();
 }
