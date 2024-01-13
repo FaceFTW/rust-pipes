70e48b79049244a1920edfc3e428872c7403db5d
Alex Westerman <alex@faceftw.dev>
2024-01-13 13:44:14 -0500

    Make build.rs cross-platform

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+#[cfg(target_os = "windows")]
 fn main() -> io::Result<()> {
     if cfg!(target_os = "windows") && !cfg!(test) {
         let mut res = winres::WindowsResource::new();
@@ -8,5 +9,10 @@ fn main() -> io::Result<()> {
         res.compile()?;
     }
 
+    Ok(())
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() -> io::Result<()> {
     Ok(())
 }
